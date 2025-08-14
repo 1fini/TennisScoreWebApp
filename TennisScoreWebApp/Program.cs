cfc075d9512890f2447635e8fd853939ec415626
Jean-Daniel GIORDANI <dan@MacBook-Pro-de-Jean-Daniel.local>
2025-08-14 22:47:18 +0200

    Init

---
+++ TennisScoreWebApp/Program.cs
@@ -0,0 +1,35 @@
+using TennisScoreWebApp.Components;
+using TennisScoreWebApp.Infrastructure.ExternalServices.TennisScoreApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Injection du HttpClient pour TennisApiClient
+builder.Services.AddHttpClient<ITennisApiClient, TennisApiClient>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7277/"); // URL de ton API
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
