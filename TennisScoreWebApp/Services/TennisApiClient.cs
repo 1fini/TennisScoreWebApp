c498daa3cf2fd7fec74cb873f8d2bfb0c1083d00
Jean-Daniel GIORDANI <dan@MacBook-Pro-de-Jean-Daniel.local>
2025-09-04 11:20:04 +0200

    feat(GUI): Add createMatch, createTournament pages

--- TennisScoreWebApp/Services/TennisApiClient.cs
+++ TennisScoreWebApp/Services/TennisApiClient.cs
@@ -108,14 +108,14 @@ namespace TennisScoreWebApp.Infrastructure.ExternalServices.TennisScoreApi
         /// <exception cref="ApiException">A server side error occurred.</exception>
         System.Threading.Tasks.Task PlayersGET2Async(System.Guid id, System.Threading.CancellationToken cancellationToken);
 
-        /// <returns>OK</returns>
+        /// <returns>Created</returns>
         /// <exception cref="ApiException">A server side error occurred.</exception>
-        System.Threading.Tasks.Task TournamentsPOSTAsync(CreateTournamentRequest body);
+        System.Threading.Tasks.Task<TournamentDto> TournamentsPOSTAsync(CreateTournamentRequest body);
 
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-        /// <returns>OK</returns>
+        /// <returns>Created</returns>
         /// <exception cref="ApiException">A server side error occurred.</exception>
-        System.Threading.Tasks.Task TournamentsPOSTAsync(CreateTournamentRequest body, System.Threading.CancellationToken cancellationToken);
+        System.Threading.Tasks.Task<TournamentDto> TournamentsPOSTAsync(CreateTournamentRequest body, System.Threading.CancellationToken cancellationToken);
 
         /// <returns>OK</returns>
         /// <exception cref="ApiException">A server side error occurred.</exception>
@@ -890,17 +890,17 @@ namespace TennisScoreWebApp.Infrastructure.ExternalServices.TennisScoreApi
             }
         }
 
-        /// <returns>OK</returns>
+        /// <returns>Created</returns>
         /// <exception cref="ApiException">A server side error occurred.</exception>
-        public virtual System.Threading.Tasks.Task TournamentsPOSTAsync(CreateTournamentRequest body)
+        public virtual System.Threading.Tasks.Task<TournamentDto> TournamentsPOSTAsync(CreateTournamentRequest body)
         {
             return TournamentsPOSTAsync(body, System.Threading.CancellationToken.None);
         }
 
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-        /// <returns>OK</returns>
+        /// <returns>Created</returns>
         /// <exception cref="ApiException">A server side error occurred.</exception>
-        public virtual async System.Threading.Tasks.Task TournamentsPOSTAsync(CreateTournamentRequest body, System.Threading.CancellationToken cancellationToken)
+        public virtual async System.Threading.Tasks.Task<TournamentDto> TournamentsPOSTAsync(CreateTournamentRequest body, System.Threading.CancellationToken cancellationToken)
         {
             var client_ = _httpClient;
             var disposeClient_ = false;
@@ -913,6 +913,7 @@ namespace TennisScoreWebApp.Infrastructure.ExternalServices.TennisScoreApi
                     content_.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/json");
                     request_.Content = content_;
                     request_.Method = new System.Net.Http.HttpMethod("POST");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"));
 
                     var urlBuilder_ = new System.Text.StringBuilder();
                     if (!string.IsNullOrEmpty(_baseUrl)) urlBuilder_.Append(_baseUrl);
@@ -942,9 +943,14 @@ namespace TennisScoreWebApp.Infrastructure.ExternalServices.TennisScoreApi
                         ProcessResponse(client_, response_);
 
                         var status_ = (int)response_.StatusCode;
-                        if (status_ == 200)
+                        if (status_ == 201)
                         {
-                            return;
+                            var objectResponse_ = await ReadObjectResponseAsync<TournamentDto>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                            if (objectResponse_.Object == null)
+                            {
+                                throw new ApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                            }
+                            return objectResponse_.Object;
                         }
                         else
                         {
@@ -1393,6 +1399,9 @@ namespace TennisScoreWebApp.Infrastructure.ExternalServices.TennisScoreApi
         [Newtonsoft.Json.JsonProperty("servingPlayerFirstName", Required = Newtonsoft.Json.Required.Always)]
         [System.ComponentModel.DataAnnotations.Required]
         public string ServingPlayerFirstName { get; set; }
+
+        [Newtonsoft.Json.JsonProperty("tournmentId", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public System.Guid? TournmentId { get; set; }
 
         [Newtonsoft.Json.JsonProperty("tournamentName", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string TournamentName { get; set; }
